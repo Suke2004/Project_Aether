d19280f567108db5b4828fe1da4f4e3b9cbdaf79
Suke2004 <lyricsofsongs96@gmail.com>
2025-12-27 13:18:49 +0530

    Built parent dashboard and analytics

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -8,5 +8,5 @@ export { default as HomeScreen } from './HomeScreen';
 export { default as QuestScreen } from './QuestScreen';
 export { default as LockScreen } from './LockScreen';
 
-// Future screens to be implemented:
-// export { default as ParentDashboard } from './ParentDashboard';
+// Parent dashboard screen
+export { default as ParentDashboard } from './ParentDashboard';
